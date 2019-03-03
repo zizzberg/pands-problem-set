1c14c80ae9fc9bd92d82d43cae0109f314042461
Zara Burton <syzygyalstudio@gmail.com>
2019-03-03 18:38:14 +0000

    test solution 1

--- solution-1.py
+++ solution-1.py
@@ -1,5 +1,9 @@
 # Solution 1 
 #declare int as z
-#line 4 - first set of brackets declares int as an input. Second brackets declare string attached to input
-z = int(input('enter a positive number:   '))
-s =  
+#line 7 - first set of brackets declares int as an input. Second brackets declare string attached to input
+#line 8 - add one to input number then times by input number and divide by 2
+#line 9 - to seperate the string and variable on print command use a comma.
+
+z = int(input("Input a number: "))
+sum = (z * (z + 1)) / 2
+  print ("you answer is: ",sum)
