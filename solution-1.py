f4724bb067d4667836150cfdd7cc4a8944bb21d8
Zara Burton <syzygyalstudio@gmail.com>
2019-03-03 16:47:04 +0000

    added input to solution 1

--- solution-1.py
+++ solution-1.py
@@ -1,3 +1,5 @@
 # Solution 1 
 #declare int as z
-z = int() 
+#line 4 - first set of brackets declares int as an input. Second brackets declare string attached to input
+z = int(input('enter a positive number:   '))
+s =  
