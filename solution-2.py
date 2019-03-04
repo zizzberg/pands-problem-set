7b3f9b021d847e582e4a4aa4ea594f55adca73a7
Zara Burton <syzygyalstudio@gmail.com>
2019-03-04 19:58:05 +0000

    added if else and outputs but not quite right! - need to check into conditional variables.

--- solution-2.py
+++ solution-2.py
@@ -1,4 +1,12 @@
 # begins with a t - 0 mon, 1 tue, 3 thursday
 
-from datetime import datetime
+import datetime
 
+dayis = datetime.datetime.today().weekday()
+
+if dayis == 1, 3: 
+    print "Today begins with a T, Taco Tuesday or Terrific Thursday"
+
+else: 
+
+    print "Today doesn't being with a T"
