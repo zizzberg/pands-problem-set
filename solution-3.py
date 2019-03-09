b7af1736f8f99869d9149733b410c3cbd01b31d8
Zara Burton <syzygyalstudio@gmail.com>
2019-03-09 16:20:15 +0000

    changed or to and (doh!)

---
+++ solution-3.py
@@ -0,0 +1,14 @@
+#define range 
+for n in range(1000, 10000):
+    for x in range(1000, n):
+        if n % 6 == 0:
+            print(n)
+            break   
+
+        else:
+
+        print(n, "is a prime number")
+
+    
+    
+    
