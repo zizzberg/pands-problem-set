7f11ec61a82c359f0675432289d30c9f6a60f53d
Zara Burton <syzygyalstudio@gmail.com>
2019-03-10 14:33:03 +0000

    started 4 - need to understand loop structure and calc within a statement for multiple conditionals.

--- solution-2.py
+++ solution-2.py
@@ -4,9 +4,8 @@ import datetime
 
 dayis = datetime.datetime.today().weekday()
 
-if dayis == 1 or 3: 
-    print ("Yay! Today starts with a T")
+if dayis == 1 and 3: 
+    print("Yay! Today starts with a T")
 
 else: 
-
-    print ("Today doesn't being with a T")
+    print("Today doesn't being with a T")
---
+++ solution-4.py
@@ -0,0 +1,10 @@
+For
+
+n = int(input("Input a positive integer: "))
+#step one if current value of n is positive divide it by 2 - if it is odd - 
+#multiply by 3 and add 1 - if 1 end program 
+If n % 2 == 0:  #even 
+else:
+n % 2 == 1: # odd
+
+print(n)
