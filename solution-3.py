2efab6e473529862d1ba50ff1fcbe9c470fc677e
Zara Burton <syzygyalstudio@gmail.com>
2019-03-10 13:07:37 +0000

    added not equal to exclude 12 from print.

--- solution-3.py
+++ solution-3.py
@@ -1,13 +1,15 @@
 #define range 
 for n in range(1000, 10000):
     for x in range(1000, n):
-        if n % 6 == 0:
+        if n % 6 == 0 and n % 12 != 0:
             print(n)
-            break   
+            break
+            
+  
 
-        else:
+    
 
-        print(n, "is a prime number")
+      
 
     
     
