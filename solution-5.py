0c136f132be3cea3b9592468fb072ed6c62211af
Zara Burton <syzygyalstudio@gmail.com>
2019-03-10 18:05:06 +0000

    started problem 5.

---
+++ solution-5.py
@@ -0,0 +1,8 @@
+#inter a positive integer and tell user whether prime is not 
+unum = int(input("Enter a positive integer to see whether it is prime: "))
+
+if unum <=1:
+    print("Not a prime")
+
+else:
+       
