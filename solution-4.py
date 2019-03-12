70e3ea3e88cc9178132cba4812b63c223d58fb4b
Zara Burton <syzygyalstudio@gmail.com>
2019-03-12 19:09:04 +0000

    added while loop and figured out operators.

--- solution-4.py
+++ solution-4.py
@@ -1,10 +1,16 @@
-For
-
-n = int(input("Input a positive integer: "))
 #step one if current value of n is positive divide it by 2 - if it is odd - 
 #multiply by 3 and add 1 - if 1 end program 
-If n % 2 == 0:  #even 
-else:
-n % 2 == 1: # odd
 
-print(n)
+n = int(input("Input a positive integer: "))
+#while loops repeat code but don't run n time - only until a condition is met
+while n != 1: 
+
+    print(n)
+
+    if n % 2 == 0:  #even
+        n = n / 2 
+    else:
+        n = n * 3 
+        n = n + 1
+
+print("1") # prints 1 at the end outside of loop to end program. 
