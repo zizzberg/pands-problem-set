3c5ba75b3b7fef229168af67f83e84321b144e26
Zara Burton <syzygyalstudio@gmail.com>
2019-03-13 20:04:02 +0000

    indentation error! fixed.

--- solution-5.py
+++ solution-5.py
@@ -3,13 +3,13 @@ unum = int(input("Enter a positive integer to see whether it is prime: "))
 
 if unum > 1:
     for i in range(2, unum): 
-        if (inum % i) == 0: # modulo = 0 = no prime
-        print (inum, "not a prime")
-        break # exit loop - next statement 
+        if (unum % i) == 0: # modulo = 0 = no prime
+            print (unum, "not a prime")
+            break # exit loop - next statement 
 
     else: 
-        print(inum, "is a prime")
+        print(unum, "is a prime")
 
 else: 
-    print(inum, "not a prime")
+    print(unum, "not a prime")
        
