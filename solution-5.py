2d02f5278b92f541795aa2e10dac072e7636b053
Zara Burton <syzygyalstudio@gmail.com>
2019-03-13 19:58:10 +0000

    added break and outside else to prime to test out.

--- solution-5.py
+++ solution-5.py
@@ -1,8 +1,15 @@
 #inter a positive integer and tell user whether prime is not 
 unum = int(input("Enter a positive integer to see whether it is prime: "))
 
-if unum <=1:
-    print("Not a prime")
+if unum > 1:
+    for i in range(2, unum): 
+        if (inum % i) == 0: # modulo = 0 = no prime
+        print (inum, "not a prime")
+        break # exit loop - next statement 
 
-else:
+    else: 
+        print(inum, "is a prime")
+
+else: 
+    print(inum, "not a prime")
        
