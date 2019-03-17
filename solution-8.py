8fb492ab79903a97d9797d3cf6df92cb344fc9a0
Zara Burton <syzygyalstudio@gmail.com>
2019-03-17 20:12:04 +0000

    added code for solution-8 - throwing strange time. Needs investigation

--- solution-8.py
+++ solution-8.py
@@ -1,5 +1,14 @@
-import datetime #import datetime 
 
-x = datetime.datetime.now()
-print(x.strftime("%A, %B, %Y, %H, %I, %p")) 
+#import datetime #import datetime 
+
+#print("%A, %B, %d, %Y, %H, %I, %p")) 
+# datetime.now().strftime
 # Converts timestamp to required format but throwing a weird mismatch with minutes. 
+
+#alt 
+import datetime
+now = datetime.datetime.today() # current date and time - incorrect time
+x = datetime.datetime.now()
+
+
+print(x.strftime("%A %B %d %Y at %H %I %p"))
